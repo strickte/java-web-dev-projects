bff71c69cfc1b381b0eee9c52e4ceeb2facc642a
strickte <166240135+strickte@users.noreply.github.com>
2024-09-19 20:24:56 -0500

    Updated for bonus missions

--- datatypes/datatypes-studio/src/main/java/org/launchcode/Area.java
+++ datatypes/datatypes-studio/src/main/java/org/launchcode/Area.java
@@ -7,10 +7,27 @@ public class Area {
     public static void main(String[] args) {
         Scanner input = new Scanner(System.in);
         System.out.println("Enter a radius: ");
+
+
         double radius = input.nextDouble();
-        double area = Circle.getArea(radius);
-        System.out.println("The area of a circle with radius " + radius + " is: " + area);
-        input.close();
+
+
+        if (radius < 0) {
+            do {
+            System.out.println("Please enter a positive number for the radius");
+            System.out.println("Enter a radius: ");
+                radius = input.nextDouble();
+        } while (radius < 0);
+            double area = Circle.getArea(radius);
+            System.out.println("The area of a circle with radius " + radius + " is: " + area);
+            input.close();
+        } else if (!input.hasNextDouble()) {
+            System.out.println("Please enter only positive integers");
+        } else {
+            double area = Circle.getArea(radius);
+            System.out.println("The area of a circle with radius " + radius + " is: " + area);
+            input.close();
+        }
     }
 
 }
